1566f777d3addb81118a791db54e827165dca595
Dan VerWeire <dverweire@gmail.com>
2012-04-18 15:57:37 -0400

    test: being preparing for running tests against multiple drivers

--- test/common.js
+++ test/common.js
@@ -1,5 +1,13 @@
 module.exports.connectionString = "DRIVER={SQLite};DATABASE=data/sqlite-test.db";
+
 module.exports.connectionObject = {
 	DRIVER : "{SQLITE}",
 	DATABASE : "data/sqlite-test.db"
 };
+
+module.exports.connections = [
+	{
+		DRIVER : "{SQLITE}",
+		DATABASE : "data/sqlite-test.db"
+	}
+];
