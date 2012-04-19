79430a55a6e4c1a7ad3896f642c63843295a18d6
Dan VerWeire <dverweire@gmail.com>
2012-04-19 11:08:44 -0400

    test: use SQLite3 instead of SQLite2

--- test/common.js
+++ test/common.js
@@ -1,13 +1,13 @@
-module.exports.connectionString = "DRIVER={SQLite};DATABASE=data/sqlite-test.db";
+module.exports.connectionString = "DRIVER={SQLite3};DATABASE=data/sqlite-test.db";
 
 module.exports.connectionObject = {
-	DRIVER : "{SQLITE}",
+	DRIVER : "{SQLITE3}",
 	DATABASE : "data/sqlite-test.db"
 };
 
 module.exports.connections = [
 	{
-		DRIVER : "{SQLITE}",
+		DRIVER : "{SQLITE3}",
 		DATABASE : "data/sqlite-test.db"
 	}
 ];
