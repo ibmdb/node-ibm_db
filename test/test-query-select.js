bb2f41652ca1fe4f3c733f28285d886dd2d05b40
Dan VerWeire <dverweire@gmail.com>
2012-11-05 12:03:27 -0500

    close database after test

--- test/test-query-select.js
+++ test/test-query-select.js
@@ -4,12 +4,13 @@ var common = require("./common")
 
 db.open(common.connectionString, function(err)
 {
-	db.query('select * from test', function (err, data) {
+	db.query('select \'a\' as test, datetime(\'now\') as nowish, * from test', function (err, data) {
 		if (err) {
 			console.error(err);
 			process.exit(1);
 		}
 		
 		console.error(data);
+		db.close(function () {});
 	});
 });
