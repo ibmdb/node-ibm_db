e06418dd51458c3051448d20675cc2cca5df6217
Dan VerWeire <dverweire@gmail.com>
2012-11-06 15:43:23 -0500

    test: add dropTables and createTables methods to common.js

--- test/common.js
+++ test/common.js
@@ -1,4 +1,6 @@
 module.exports.connectionString = "DRIVER={SQLite3};DATABASE=data/sqlite-test.db";
+//module.exports.connectionString = "DRIVER={MySQL};DATABASE=test;HOST=localhost;USER=test;";
+//module.exports.connectionString = process.env.ODBC_CONNETION_STRING;
 
 module.exports.connectionObject = {
 	DRIVER : "{SQLITE3}",
@@ -11,3 +13,11 @@ module.exports.connections = [
 		DATABASE : "data/sqlite-test.db"
 	}
 ];
+
+module.exports.dropTables = function (db, cb) {
+  db.query("drop table TEST", cb);
+};
+
+module.exports.createTables = function (db, cb) {
+  db.query("create table TEST(COLINT INTEGER, COLDATETIME DATETIME, COLTEXT TEXT)", cb);
+};
