8f740569f99b9c8b5e945802045e404c748d712e
Dan VerWeire <dverweire@gmail.com>
2012-11-07 18:23:47 -0500

    test: add failing tests for parameters, only strings are currently working

---
+++ test/test-param-select-with-decimals-only.js
@@ -0,0 +1,20 @@
+var common = require("./common")
+  , odbc = require("../odbc.js")
+  , db = new odbc.Database()
+  , assert = require("assert");
+
+
+db.open(common.connectionString, function (err) {
+  assert.equal(err, null);
+  
+  db.query("select ? as DECCOL1 "
+    , [5.5]
+    , function (err, data, more) {
+        db.close(function () {
+          assert.equal(err, null);
+          assert.deepEqual(data, [{
+            DECCOL1: 5.5
+          }]);
+        });
+    });
+});
