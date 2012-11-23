029cec637f326b67595fa50e6e06d71248cc9f0f
Dan VerWeire <dverweire@gmail.com>
2012-11-23 11:36:58 -0500

    test: added test for binding parameters with a null value

---
+++ test/test-param-select-with-nulls-mixed.js
@@ -0,0 +1,22 @@
+var common = require("./common")
+  , odbc = require("../odbc.js")
+  , db = new odbc.Database()
+  , assert = require("assert");
+
+
+db.open(common.connectionString, function (err) {
+  assert.equal(err, null);
+  
+  db.query("select ? as TEXTCOL1, ? as TEXTCOL2, ? as NULLCOL1 "
+    , ["something", "something", null]
+    , function (err, data, more) {
+        db.close(function () {
+          assert.equal(err, null);
+          assert.deepEqual(data, [{
+            TEXTCOL1: "something",
+            TEXTCOL2: "something",
+            NULLCOL1: null 
+          }]);
+        });
+    });
+});
