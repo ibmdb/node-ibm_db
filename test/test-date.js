165914636f1068cb90d60acdc50a1199db30769a
Dan VerWeire <dverweire@gmail.com>
2012-11-23 17:16:49 -0500

    test: log some things in the date test

--- test/test-date.js
+++ test/test-date.js
@@ -9,18 +9,23 @@ db.open(common.connectionString, function(err) {
   assert.equal(db.connected, true);
   
   var dt = new Date();
+  var sql = "SELECT cast('" + dt.toISOString().replace('Z','') + "' as datetime) as DT1";
   
-  db.query("SELECT cast('" + dt.toISOString().replace('Z','') + "' as datetime) as DT1", function (err, data) {
+  console.log(sql);
+  
+  db.query(sql, function (err, data) {
     assert.equal(err, null);
     assert.equal(data.length, 1);
 
     db.close(function () {
       assert.equal(db.connected, false);
+      console.log(dt);
+      console.log(data);
       
       //test selected data after the connection
       //is closed, in case the assertion fails
       assert.equal(data[0].DT1.constructor.name, "Date", "DT1 is not an instance of a Date object");
-      assert.equal(data[0].DT1.getTime(), dt.getTime(), "The original time passed to the database is not the same as the time we got back");
+      assert.equal(data[0].DT1.getTime(), dt.getTime());
     });
   });
 });
