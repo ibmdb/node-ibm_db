8bee10df15e65c6421ebfcddab1fcdf56d9d3f37
Dan VerWeire <dverweire@gmail.com>
2013-04-02 15:18:40 -0400

    test: use gc() to try to clear objects when testing for leaks

--- test/test-memory-leaks-new-objects.js
+++ test/test-memory-leaks-new-objects.js
@@ -11,6 +11,20 @@ start = process.memoryUsage().heapUsed;
 for (x = 0; x < openCount; x++ ) {
     (function () {
         var db = new odbc.Database();
+        db = null;
+    })();
+}
+
+gc();
+
+console.log(process.memoryUsage().heapUsed - start);
+
+gc();
+
+for (x = 0; x < openCount; x++ ) {
+    (function () {
+        var db = new odbc.Database();
+        db = null;
     })();
 }
 
