1c75b7421c452d9bbc7b248f504ffaf969645d7b
Dan VerWeire <dverweire@gmail.com>
2013-06-30 23:17:44 -0400

    New and updated unicode tests

--- test/test-param-select-with-unicode.js
+++ test/test-param-select-with-unicode.js
@@ -1,11 +1,11 @@
-var common = require("./common")
+﻿var common = require("./common")
   , odbc = require("../")
   , db = new odbc.Database()
   , assert = require("assert")
   ;
 
 db.open(common.connectionString, function(err) {
-  db.query("select '車' as UNICODETEXT", function (err, data) {
+  db.query("select ? as UNICODETEXT", ['ף צ ץ ק ר ש תכ ך ל מ ם נ ן ס ע פ 電电電買买買開开開東东東車车車'], function (err, data) {
     db.close(function () {
       console.log(data);
       assert.equal(err, null);
