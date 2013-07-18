48c0bbf601b0ba2be8fc2bd114cec814e6c9983a
Dan VerWeire <dverweire@gmail.com>
2013-07-18 11:49:30 -0400

    Add test for binding a long string

---
+++ test/test-prepare-bind-execute-long-string.js
@@ -0,0 +1,66 @@
+var common = require("./common")
+  , odbc = require("../")
+  , db = new odbc.Database()
+  , assert = require("assert")
+  ;
+
+db.open(common.connectionString, function(err){ 
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  
+  issueQuery();
+});
+
+function issueQuery() {
+  var count = 0
+    , time = new Date().getTime()
+    , stmt
+    , result
+    , data
+    , str = ''
+    ;
+  
+  for (var x = 0; x < 10000; x++) {
+    str += 't';
+  }
+  
+  assert.doesNotThrow(function () {
+    stmt = db.prepareSync('select ? as longString');
+  });
+  
+  assert.doesNotThrow(function () {
+    stmt.bindSync([str]);
+  });
+  
+  assert.doesNotThrow(function () {
+    result = stmt.executeSync();
+  });
+  
+  assert.doesNotThrow(function () {
+    data = result.fetchAllSync();
+  });
+  
+//   console.log(data);
+  console.log(str.length);
+  console.log(data[0].longString.length);
+  
+//   for (var x = 0; x < str.length; x++) {
+//     if (str[x] != data[0].longString[x]) {
+//       //console.log(x, str[x], data[0].longString[x]);
+//     }
+//   }
+  
+  assert.equal(data[0].longString, str);
+  
+  finish(0);
+}
+
+function finish(exitCode) {
+  db.close(function () {
+    console.log("connection closed");
+    
+    process.exit(exitCode || 0);
+  });
+}
