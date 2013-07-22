74c87245bd890a8d413b7620a9ffa78a6fcb5aa3
Dan VerWeire <dverweire@gmail.com>
2013-07-22 17:46:07 -0400

    test: add test for domains

---
+++ test/test-domains-open.js
@@ -0,0 +1,19 @@
+var domain = require("domain");
+
+var d = domain.create();
+
+d.on("error", function (error) {
+  console.log("Error caught!", error);
+});
+
+d.run(function() {
+   var db = require("../")();
+
+   console.trace();
+
+   db.open("wrongConnectionString", function (error) {
+      console.trace();
+
+      throw new Error();
+    });
+});
