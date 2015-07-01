b055524c946ad753c796cefeb338d059db164747
Bimal Jha <bimaljha@in.ibm.com>
2015-07-01 14:23:46 +0530

    Add pool related test files.

---
+++ test/test-pool-rollbackTransaction.js
@@ -0,0 +1,30 @@
+var common = require("./common")
+	, odbc = require("../")
+	, pool = new odbc.Pool()
+	, cn = common.connectionString
+
+
+pool.open(cn, function (err, conn) {
+  if (err) {
+    return console.log(err);
+  }
+  conn.beginTransaction(function (err) {
+    if (err) {
+      //could not begin a transaction for some reason. 
+      console.log(err);
+      return conn.closeSync();
+    }
+    conn.querySync("create table mytab4 (c1 int, c2 varchar(20))");
+    conn.querySync("insert into mytab4 values( 3, 'bimal')");
+    console.log(conn.querySync("select * from mytab4"));
+    conn.rollbackTransaction(function (err) {
+      if (err) {
+        //error during commit 
+        console.log(err);
+        return conn.closeSync();
+      }
+    });
+  });
+});
+
+
---
+++ test/test-pool-uncommited.js
@@ -0,0 +1,29 @@
+var common = require("./common")
+	, ibmdb = require("../")
+	, pool = new ibmdb.Pool()
+	, cn = common.connectionString
+
+
+var request =  function (err, conn) {
+  if (err) {
+    return console.log(err);
+  }
+  conn.beginTransaction(function (err) {
+    if (err) {
+      //could not begin a transaction for some reason. 
+      console.log(err);
+      return conn.closeSync();
+    }
+    conn.querySync("create table mytab4 (c1 int, c2 varchar(20))");
+    conn.querySync("insert into mytab4 values( 3, 'bimal')");
+    console.log(conn.querySync("select * from mytab4"));
+    conn.close(function(){});
+  });
+}
+
+pool.open(cn, request);
+pool.open(cn, request);
+setTimeout(function() {
+    pool.open(cn, request);
+    pool.open(cn, request);
+}, 2000);
