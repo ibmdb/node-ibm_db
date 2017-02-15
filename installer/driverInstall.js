26a7c4b7084803b53620d1723e4886e0abde0aec
rhtpandeyIN <rhtpandey@in.ibm.com>
2017-02-15 18:35:28 +0530

    refractor of code

--- installer/driverInstall.js
+++ installer/driverInstall.js
@@ -10,11 +10,9 @@ var exec = require('child_process').exec;
 var request = require('request');
 
 var installerURL = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli';
-var license_agreement = '\n****************************************\nYou are downloading a package which includes the Node.js module for IBM DB2/Informix.  The module is licensed under the Apache License 2.0. The package also includes IBM ODBC and CLI Driver from IBM, which is automatically downloaded as the node module is installed on your system/device. The license agreement to the IBM ODBC and CLI Driver is available in '+DOWNLOAD_DIR+'   Check for additional dependencies, which may come with their own license agreement(s). Your use of the components of the package and dependencies constitutes your acceptance of their respective license agreements. If you do not accept the terms of any license agreement(s), then delete the relevant component(s) from your device.\n****************************************\n';
-
 var CURRENT_DIR = process.cwd();
 var DOWNLOAD_DIR = path.resolve(CURRENT_DIR, 'installer');
-var INSTALLER_FILE;
+var INSTALLER_FILE; 
 installerURL = process.env.IBM_DB_INSTALLER_URL || installerURL;
 installerURL = installerURL + "/";
 
@@ -26,62 +24,63 @@ var install_node_ibm_db = function(file_url) {
     var arch = os.arch();
     var endian = os.endianness();
     var installerfileURL;
-
+    
     var fstream = require('fstream');
     var unzipper = require('unzipper');
-
+                
     var IBM_DB_HOME, IBM_DB_INCLUDE, IBM_DB_LIB, IBM_DB_DIR;
-
-    if (arch !== 'x64' && (platform === 'darwin' || platform === 'win32')) {
-        console.log('32bit', (platform === 'darwin' ? 'Mac OS' : 'Windows'), 'not supported, exiting...');
-        process.exit(1);
-    }
-
+    
     if(platform == 'win32') {
-        var BUILD_FILE = path.resolve(CURRENT_DIR, 'build.zip');
+        if(arch == 'x64') {
+            var BUILD_FILE = path.resolve(CURRENT_DIR, 'build.zip');
 
-        //Windows node binary names should update here.
-        var ODBC_BINDINGS = 'build\/Release\/odbc_bindings.node';
-        var ODBC_BINDINGS_V10 = 'build\/Release\/odbc_bindings.node.0.10.36';
-        var ODBC_BINDINGS_V12 = 'build\/Release\/odbc_bindings.node.0.12.7';
-        var ODBC_BINDINGS_V4 = 'build\/Release\/odbc_bindings.node.4.6.1';
-        var ODBC_BINDINGS_V6 = 'build\/Release\/odbc_bindings.node.6.9.1';
+            //Windows node binary names should update here.
+            var ODBC_BINDINGS = 'build\/Release\/odbc_bindings.node';
+            var ODBC_BINDINGS_V10 = 'build\/Release\/odbc_bindings.node.0.10.36';
+            var ODBC_BINDINGS_V12 = 'build\/Release\/odbc_bindings.node.0.12.7';
+            var ODBC_BINDINGS_V4 = 'build\/Release\/odbc_bindings.node.4.6.1';
+            var ODBC_BINDINGS_V6 = 'build\/Release\/odbc_bindings.node.6.9.1';
 
-        /*
-         * odbcBindingsNode will consist of the node binary-
-         * file name according to the node version in the system.
-         */
-        var odbcBindingsNode = (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 0.12) && ODBC_BINDINGS_V10 ||
-        (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 4.0) && ODBC_BINDINGS_V12  ||
-        (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 5.0) && ODBC_BINDINGS_V4 ||
-        (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 7.0) && ODBC_BINDINGS_V6 || ODBC_BINDINGS ;
+            /*
+             * odbcBindingsNode will consist of the node binary-
+             * file name according to the node version in the system.
+             */
+            var odbcBindingsNode = (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 0.12) && ODBC_BINDINGS_V10 ||
+            (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 4.0) && ODBC_BINDINGS_V12  ||
+            (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 5.0) && ODBC_BINDINGS_V4 ||
+            (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 7.0) && ODBC_BINDINGS_V6 || ODBC_BINDINGS ;
 
-        readStream = fs.createReadStream(BUILD_FILE);
+            readStream = fs.createReadStream(BUILD_FILE);
 
-        /*
-         * unzipper will parse the build.zip file content and
-         * then it will check for the odbcBindingsNode
-         * (node Binary), when it gets that binary file,
-         * fstream.Writer will write the same node binary
-         * but the name will be odbc_bindings.node, and the other
-         * binary files and build.zip will be discarded.
-         */
-        readStream.pipe(unzipper.Parse())
-        .on('entry', function (entry) {
-            if(entry.path === odbcBindingsNode) {
-                entry.pipe(fstream.Writer(ODBC_BINDINGS));
-            } else {
-                entry.autodrain();
-            }
-        })
-        .on('error', function(e) {
-            console.log('error',e);
-        })
-	      .on('finish', function() {
-            fs.unlinkSync(BUILD_FILE);
-	      });
+            /*
+             * unzipper will parse the build.zip file content and
+             * then it will check for the odbcBindingsNode
+             * (node Binary), when it gets that binary file,
+             * fstream.Writer will write the same node binary
+             * but the name will be odbc_bindings.node, and the other
+             * binary files and build.zip will be discarded.
+             */
+            readStream.pipe(unzipper.Parse())
+            .on('entry', function (entry) {
+                if(entry.path === odbcBindingsNode){
+                    entry.pipe(fstream.Writer(ODBC_BINDINGS));
+                } else {
+                    entry.autodrain();
+                }
+            })
+            .on('error', function(e) {
+                console.log('error',e);
+            })
+            .on('finish', function() {
+                fs.unlinkSync(BUILD_FILE);
+            });
 
-        removeUsedPackages();
+            removeUsedPackages();
+        } else {
+            console.log('Windows 32 bit not supported. Please use an ' +
+                        'x64 architecture.');
+            return;
+        }
     }
 
     /*
@@ -92,7 +91,7 @@ var install_node_ibm_db = function(file_url) {
      * ELSE: platform specific compressed clidriver package will be download
      * and then extract for further use.
      */
-    if(process.env.IBM_DB_HOME)
+    if(process.env.IBM_DB_HOME) 
     {
         IBM_DB_HOME = process.env.IBM_DB_HOME;
         IBM_DB_INCLUDE = path.resolve(IBM_DB_HOME, 'include');
@@ -104,27 +103,26 @@ var install_node_ibm_db = function(file_url) {
            IBM_DB_LIB = path.resolve(IBM_DB_HOME, 'lib');
         }
         console.log('IBM_DB_HOME environment variable have already been set to '+IBM_DB_HOME);
-
+        
         if (!fs.existsSync(IBM_DB_HOME)) {
-            console.log(IBM_DB_HOME + ' directory does not exist. Please check if you have ' +
+            console.log(IBM_DB_HOME + ' directory does not exist. Please check if you have ' + 
                         'set the IBM_DB_HOME environment variable\'s value correctly.');
         }
-
+        
         if (!fs.existsSync(IBM_DB_INCLUDE)) {
-            console.log(IBM_DB_INCLUDE + ' directory does not exist. Please check if you have ' +
+            console.log(IBM_DB_INCLUDE + ' directory does not exist. Please check if you have ' + 
                         'set the IBM_DB_HOME environment variable\'s value correctly.');
         }
-
+        
         if (!fs.existsSync(IBM_DB_LIB)) {
-            console.log(IBM_DB_LIB + ' directory does not exist. Please check if you have ' +
+            console.log(IBM_DB_LIB + ' directory does not exist. Please check if you have ' + 
                         'set the IBM_DB_HOME environment variable\'s value correctly.');
         }
-
         if( platform != 'win32') {
             if(!fs.existsSync(IBM_DB_HOME + "/lib"))
                 fs.symlinkSync(IBM_DB_LIB, path.resolve(IBM_DB_HOME, 'lib'));
 
-            if((platform == 'linux') || (platform =='aix') ||
+            if((platform == 'linux') || (platform =='aix') || 
                (platform == 'darwin' && arch == 'x64')) {
                 removeWinBuildArchive();
                 buildBinary(false);
@@ -134,10 +132,16 @@ var install_node_ibm_db = function(file_url) {
             }
         }
     } else {
-        if(platform == 'win32') {
-            installerfileURL = installerURL + 'ntx64_odbc_cli.zip';
-
-        } else if(platform == 'linux') {
+        if(platform == 'win32') 
+        {
+            if(arch == 'x64') {
+                installerfileURL = installerURL + 'ntx64_odbc_cli.zip';
+            }/* else {
+                installerfileURL = installerURL + 'nt32_odbc_cli.zip';
+            }*/
+        } 
+        else if(platform == 'linux') 
+        {
             if(arch == 'x64') {
                 installerfileURL = installerURL + 'linuxx64_odbc_cli.tar.gz';
             } else if(arch == 's390x') {
@@ -154,33 +158,48 @@ var install_node_ibm_db = function(file_url) {
             } else {
                 installerfileURL = installerURL + 'linuxia32_odbc_cli.tar.gz';
             }
-
-        } else if(platform == 'aix') {
-            if(arch == 'ppc') {
-                installerfileURL = installerURL + 'aix32_odbc_cli.tar.gz';
+        } 
+        else if(platform == 'darwin') 
+        {
+            if(arch == 'x64') {
+                installerfileURL = installerURL + 'macos64_odbc_cli.tar.gz';
             } else {
+                console.log('Mac OS 32 bit not supported. Please use an ' +
+                            'x64 architecture.');
+                return;
+            }
+        } 
+        else if(platform == 'aix')
+        {
+            if(arch == 'ppc')
+            {
+                installerfileURL = installerURL + 'aix32_odbc_cli.tar.gz';
+            }
+            else
+            {
                 installerfileURL = installerURL + 'aix64_odbc_cli.tar.gz';
             }
-
-        } else if(platform == 'darwin') {
-            installerfileURL = installerURL + 'macos64_odbc_cli.tar.gz';
-
-        } else {
-            installerfileURL = installerURL + platform + arch + '_odbc_cli.tar.gz';
         }
-
+        else 
+        {
+            installerfileURL = installerURL + platform + arch + 
+                               '_odbc_cli.tar.gz';
+        }
+        
         if(!installerfileURL) {
             console.log('Unable to fetch driver download file. Exiting the ' +
                         'install process.');
             process.exit(1);
         }
+        
+        var license_agreement = '\n****************************************\nYou are downloading a package which includes the Node.js module for IBM DB2/Informix.  The module is licensed under the Apache License 2.0. The package also includes IBM ODBC and CLI Driver from IBM, which is automatically downloaded as the node module is installed on your system/device. The license agreement to the IBM ODBC and CLI Driver is available in '+DOWNLOAD_DIR+'   Check for additional dependencies, which may come with their own license agreement(s). Your use of the components of the package and dependencies constitutes your acceptance of their respective license agreements. If you do not accept the terms of any license agreement(s), then delete the relevant component(s) from your device.\n****************************************\n';
 
         var file_name = url.parse(installerfileURL).pathname.split('/').pop();
         INSTALLER_FILE = path.resolve(DOWNLOAD_DIR, file_name);
-
+        
         console.log('Downloading DB2 ODBC CLI Driver from ' +
                     installerfileURL+'...');
-
+                    
         fs.stat(installerfileURL, function (err, stats) {
             if (!err && stats.isFile()) {
                 INSTALLER_FILE = installerfileURL;
@@ -190,7 +209,7 @@ var install_node_ibm_db = function(file_url) {
         });
 
     }  // * END OF EXECUTION */
-
+ 
     function copyAndExtractDriver() {
         if(platform == 'win32') {
             readStream = fs.createReadStream(INSTALLER_FILE);
@@ -213,8 +232,8 @@ var install_node_ibm_db = function(file_url) {
             extractCLIDriver.on('err', function() {
                 console.log(err);
             });
-        }
-        else
+        } 
+        else 
         {
             var targz = require('targz');
             var compress = targz.decompress({src: INSTALLER_FILE, dest: DOWNLOAD_DIR}, function(err){
@@ -235,14 +254,15 @@ var install_node_ibm_db = function(file_url) {
         }
     }
 
-    function buildBinary(isDownloaded) {
+    function buildBinary(isDownloaded) 
+    {
         var buildString = "node-gyp configure build --IBM_DB_HOME=\"$IBM_DB_HOME\"";
         if(isDownloaded) {
             buildString = buildString + " --IS_DOWNLOADED=true";
         } else {
             buildString = buildString + " --IS_DOWNLOADED=false";
         }
-        if( platform == 'win32')
+        if( platform == 'win32') 
         {
             buildString = buildString + " --IBM_DB_HOME_WIN=%IBM_DB_HOME%";
         }
@@ -253,11 +273,11 @@ var install_node_ibm_db = function(file_url) {
                 process.exit(1);
             }
 
-            if(platform == 'darwin' && arch == 'x64')
+            if(platform == 'darwin' && arch == 'x64') 
             {
                 // Run the install_name_tool
                 var nameToolCommand = "install_name_tool -change libdb2.dylib $IBM_DB_HOME/lib/libdb2.dylib ./build/Release/odbc_bindings.node" ;
-                var nameToolCmdProcess = exec(nameToolCommand ,
+                var nameToolCmdProcess = exec(nameToolCommand , 
                   function (error1, stdout1, stderr1) {
                     if (error1 !== null) {
                         console.log('Error setting up the lib path to ' +
@@ -269,8 +289,9 @@ var install_node_ibm_db = function(file_url) {
             removeUsedPackages();
         });
     } //buildBinary
-
-    function removeUsedPackages() {
+    
+    function removeUsedPackages()
+    {
         var packages = ["nan", "fstream", "unzipper", "targz"];
         for( var index = 0; index < packages.length; index++ )
         {
@@ -286,17 +307,18 @@ var install_node_ibm_db = function(file_url) {
         }
     }
 
-    function removeWinBuildArchive() {
+    function removeWinBuildArchive() 
+    {
         var WIN_BUILD_FILE = path.resolve(CURRENT_DIR, 'build.zip');
-        fs.exists(WIN_BUILD_FILE, function(exists)
+        fs.exists(WIN_BUILD_FILE, function(exists) 
         {
-            if (exists)
+            if (exists) 
             {
                 fs.unlinkSync(WIN_BUILD_FILE);
             }
         });
     }
-
+    
     // Function to download clidriver file using request module.
     function getInstallerFile(installerfileURL) {
         var outStream = fs.createWriteStream(INSTALLER_FILE);
@@ -306,6 +328,7 @@ var install_node_ibm_db = function(file_url) {
             cosole.log(err);
         });
     };
-}//install_node_ibm_db
+
+}; //install_node_ibm_db
 
 install_node_ibm_db();
