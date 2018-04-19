752f4f16f2868e5b58b72aede7b687e91e04fed4
Rohit Pandey <rpandey@rocketsoftware.com>
2018-04-19 16:47:28 +0530

    Updated test case for issue #357 (#380)
    * Updated test case for issue #357, #308 , #379 
    * adding fix for issue #296

--- test/test-blob-file.js
+++ test/test-blob-file.js
@@ -8,10 +8,8 @@ var common = require("./common")
     , fs = require('fs')
     , cn = common.connectionString;
 
-ibmdb.open(cn, function (err,conn) 
-{
-  if (err) 
-  {
+ibmdb.open(cn, function (err,conn) {
+  if (err) {
     console.log(err);
     process.exit(-1);
   }
@@ -19,22 +17,24 @@ ibmdb.open(cn, function (err,conn)
       conn.querySync("drop table mytab");
   } catch (e) {};
   try {
-    conn.querySync("create table mytab (empId int, photo BLOB(1M), trace CLOB(1M))");
-    } catch (e) {};
+    conn.querySync("create table mytab (empId int, photo BLOB(1M), trace CLOB(1M), document BLOB(1M))");
+  } catch (e) {};
   
-  var img1= __dirname + '/data/phool.jpg'; //fs.readFileSync('phool.jpg','binary');
-  var text= __dirname + '/data/trc.fmt'; //fs.readFileSync('trc.fmt','ascii');
+  var img1 = __dirname + '/data/phool.jpg'; //fs.readFileSync('phool.jpg','binary');
+  var text = __dirname + '/data/trc.fmt'; //fs.readFileSync('trc.fmt','ascii');
+  var doc  = __dirname + '/data/sampledoc.docx'; 
 
   var len1  = fs.statSync(img1)["size"];
   var len2  = fs.statSync(text)["size"];
+  var len3  = fs.statSync(doc)["size"];
+
   console.log( "img1.length = " + len1);
   console.log( "text.length = " + len2);
+  console.log( "doc.length = " +  len3);
 
-  conn.prepare("insert into mytab(empId, photo, trace) VALUES (?, ?, ?)", 
-      function (err, stmt) 
-   {
-    if (err) 
-    {
+  conn.prepare("insert into mytab(empId, photo, trace, document) VALUES (?, ?, ?, ?)",
+  function (err, stmt) {
+    if (err) {
       console.log(err);
       return conn.closeSync();
     }
@@ -44,45 +44,47 @@ ibmdb.open(cn, function (err,conn)
     // must be passed as JSON Object or Array.
     var photo = {ParamType:"FILE", DataType: "BLOB", "Data":img1};
     var tracefile = {ParamType: 3, "DataType": "CLOB", Data: text};
+    var document = {ParamType:"FILE", DataType: "BLOB", "Data":doc};
 
-	stmt.execute([18, photo, tracefile], function (err, result) 
-	{
-      if( err ) console.log(err);  
+    stmt.execute([18, photo, tracefile, document],
+    function (err, result) {
+      if( err ) console.log(err);
       else result.closeSync();
-	  
-	  conn.prepare("select * from mytab", function (err, stmt)
-      {
-        if(err) 
-        {
+
+      conn.prepare("select * from mytab", function (err, stmt) {
+        if(err) {
           console.log(err);
           return conn.closeSync();
         }
 
         stmt.execute([], function(err, result) {
           if(err) console.log(err);
-          else 
-          {
+          else {
             data = result.fetchAllSync();
             fs.writeFileSync('phool2.jpg', data[0].PHOTO, 'binary');
             fs.writeFileSync('trc2.fmt', data[0].TRACE, 'ascii');
+            fs.writeFileSync('doc2.docx', data[0].DOCUMENT, 'binary');
+
             try {
-                conn.querySync("drop table mytab");
+              conn.querySync("drop table mytab");
             } catch (e) {};
-            result.closeSync();
   
+            result.closeSync();
             var size1 = fs.statSync("phool2.jpg")["size"];
             var size2 = fs.statSync("trc2.fmt")["size"];
-            console.log("Lengths after select = " + size1+ ", " + size2);
+            var size3 = fs.statSync("doc2.docx")["size"];
+
+            console.log("Lengths after select = " + size1 + ", " + size2 + ", " + size3);
             assert(len1, size1);
             assert(len2, size2);
+            assert(len3, size3);
 
             fs.unlinkSync("phool2.jpg");
+            fs.unlinkSync("doc2.docx");
             fs.unlink("trc2.fmt", function () { console.log('done'); });
           }
         });
       });
-	});
+    });
   });
 });
-
-
--- test/test-blob-insert.js
+++ test/test-blob-insert.js
@@ -2,44 +2,42 @@
 // First read the file into buffer and then pass buffer to ibm_db.
 // Author : bimaljha@in.ibm.com
 
-var common = require("./common")
-    , ibmdb = require("../")
-    , assert = require("assert")
-    , fs = require('fs')
-    , cn = common.connectionString
-    , inputfile1 = 'data/phool.jpg'
-    , inputfile2 = 'data/trc.fmt'
-    , outputfile1 = 'phool2.jpg'
-    , outputfile2 = 'trc2.fmt'
-    ;
+var common = require("./common"),
+    ibmdb = require("../"),
+    assert = require("assert"),
+    fs = require('fs'),
+    cn = common.connectionString,
+    inputfile1 = 'data/phool.jpg',
+    inputfile2 = 'data/trc.fmt',
+    outputfile1 = 'phool2.jpg',
+    outputfile2 = 'trc2.fmt';
 
-ibmdb.open(cn, function (err,conn) 
-{
-  if (err) 
-  {
+ibmdb.open(cn, function (err,conn) {
+  if (err) {
     console.log(err);
   }
   assert.equal(err, null);
   try {
-      conn.querySync("drop table mytab");
+    conn.querySync("drop table mytab");
   } catch (e) {};
   try {
-    conn.querySync("create table mytab (empId int, photo BLOB(1M), trace CLOB(1M))");
-    } catch (e) {};
+    conn.querySync("create table mytab (empId int, photo BLOB(1M), trace CLOB(1M), buffer BLOB(1M))");
+  } catch (e) {};
   
   var img1= fs.readFileSync(inputfile1,'binary');
   var text= fs.readFileSync(inputfile2,'ascii');
+  var buf = Buffer.from('49 49 2A 00 C8 8C 00 00 73 C5 3C 13 83 39 98 35', 'binary');
+  var bufferInsert = buf.toString();
 
   var len1 = img1.length;
   var len2 = text.length;
   console.log( "img1.length = " + len1);
   console.log( "text.length = " + len2);
+  console.log("buffer data = "  + bufferInsert);
 
-  conn.prepare("insert into mytab(empId, photo, trace) VALUES (?, ?, ?)", 
-      function (err, stmt) 
-   {
-    if (err) 
-    {
+  conn.prepare("insert into mytab(empId, photo, trace, buffer) VALUES (?, ?, ?, ?)",
+  function (err, stmt) {
+    if (err) {
       console.log(err);
       return conn.closeSync();
     }
@@ -55,45 +53,46 @@ ibmdb.open(cn, function (err,conn)
      */
     var photo = {ParamType:1, DataType: "BLOB", "Data":img1};
     var tracefile = {DataType: "CLOB", Data: text};
+    var buffer = {CType: "BLOB", DataType: "BLOB", Data: buf};
 
-	stmt.execute([18, photo, tracefile], function (err, result) 
-	{
+    stmt.execute([18, photo, tracefile, buffer], function (err, result) {
       if( err ) console.log(err);  
       else result.closeSync();
-	  
-	  conn.prepare("select * from mytab", function (err, stmt)
-      {
-        if(err) 
-        {
+
+      conn.prepare("select * from mytab", function (err, stmt) {
+        if(err) {
           console.log(err);
           return conn.closeSync();
         }
 
         stmt.execute([], function(err, result) {
           if(err) console.log(err);
-          else 
-          {
+          else {
             data = result.fetchAllSync();
             fs.writeFileSync(outputfile1, data[0].PHOTO, 'binary');
             fs.writeFileSync(outputfile2, data[0].TRACE, 'ascii');
+            var bufferReturn = data[0].BUFFER;
+
             try {
-                conn.querySync("drop table mytab");
+              conn.querySync("drop table mytab");
             } catch (e) {};
             result.closeSync();
   
             var size1 = fs.statSync(outputfile1)["size"];
             var size2 = fs.statSync(outputfile2)["size"];
+
             console.log("Lengths after select = " + size1+ ", " + size2);
+            console.log("buffer after select : " + bufferReturn);
+
             assert(len1, size1);
             assert(len2, size2);
+            assert(bufferInsert, bufferReturn);
 
             fs.unlinkSync(outputfile1);
             fs.unlink(outputfile2, function () { console.log('done'); });
           }
         });
       });
-	});
+    });
   });
 });
-
-
