75cf66004232d071e7f76c7acfd7672db8f8e759
Bimal Jha <bjha@rocketsoftware.com>
2018-05-09 21:14:43 +0530

    Update test file for issue #388

--- test/test-basic-test.js
+++ test/test-basic-test.js
@@ -13,6 +13,7 @@ ibmdb.open(cn, {"fetchMode": 3}, function(err, conn) { // 3 means FETCH_ARRARY
     conn.querySync("drop table mytab1");
     } catch (e) {}
   conn.querySync("create table mytab1 (c1 int, c2 varchar(10))");
+  conn.querySync("insert into mytab1 values ( 4, 'für')");
   conn.query('select 1, 4, 5 from sysibm.sysdummy1;' +
              'select * from mytab1 where c1 = 2;'+
              'select 3,7,8 from sysibm.sysdummy1', [23], function (err, data) {
@@ -21,6 +22,8 @@ ibmdb.open(cn, {"fetchMode": 3}, function(err, conn) { // 3 means FETCH_ARRARY
       console.log( data);
     }
   });
+  var d=conn.querySync("select * from mytab1 where (UCASE(C2) LIKE '%FUR%' OR UCASE(C2) LIKE '%FüR%') FOR READ ONLY WITH UR OPTIMIZE FOR 50 ROWS");
+  console.log("Selected data with special character  = ", d);
   conn.prepare("insert into mytab1 VALUES (?, ?)", function (err, stmt) {
     if (err) {
       //could not prepare for some reason
@@ -48,7 +51,7 @@ ibmdb.open(cn, {"fetchMode": 3}, function(err, conn) { // 3 means FETCH_ARRARY
             console.log("Fetched Data = " );
             console.log(data);
             result.closeSync();
-            assert.deepEqual(data, [ { C1: 34245, C2: 'bimal' } ]);
+            assert.deepEqual(data, [ { C1: 4, C2: 'für' }, { C1: 34245, C2: 'bimal' } ]);
             var valueArray = [];
             valueArray.push(34245);
             data = conn.querySync('select * from mytab1 where c1 = ?;', valueArray);
