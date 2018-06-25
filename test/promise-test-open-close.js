3f2679238b8ca164b9294a63c55bb413fa611fd7
Bimal Jha <bjha@rocketsoftware.com>
2018-06-25 11:45:27 -0400

    update test file for issue #415

--- test/promise-test-open-close.js
+++ test/promise-test-open-close.js
@@ -1,26 +1,34 @@
 var common = require("./common")
-  , odbc = require("../")
-  , db = new odbc.Database()
+  , ibmdb = require("../")
+  , db = new ibmdb.Database()
   , assert = require("assert");
-
-//assert.equal(db.connected, false);
 
 db.query("select * from " + common.tableName).then(null, function (err) {
   assert.deepEqual(err, { message: 'Connection not open.' });
   assert.equal(db.connected, false);
 });
 
-
-
 db.open(common.connectionString).then(function(res) {
+  console.log("db = ", db);
   assert.equal(db.connected, true);
-
-  db.close().then(function () {
-    assert.equal(db.connected, false);
-
-    db.query("select * from " + common.tableName).then(null, function (err) {
-      assert.deepEqual(err, { message: 'Connection not open.' });
+  db.query("select * from " + common.tableName).then(null, function (err) {
+	var errorFound = false;
+	if(err) console.log(err);
+	if (err.message) {
+	    var errorFound = err.message.includes("SQL0204N");
+	}
+    assert.equal(errorFound, true);
+    assert.equal(db.connected, true);
+    db.close().then(function () {
       assert.equal(db.connected, false);
+
+      db.query("select * from " + common.tableName).then(null, function (err) {
+	    if(err) console.log(err);
+        assert.deepEqual(err, { message: 'Connection not open.' });
+        assert.equal(db.connected, false);
+      });
     });
   });
-});
+})
+.catch(function(err){console.log(err);})
+.done();
