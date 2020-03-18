047f67d0b3a370ee31cff145d759c1b017ed648f
Ravuri Sai Ram Akhil <rakhil@rocketsoftware.com>
2020-03-18 11:07:11 +0530

    Added version detect code in findElectronversion()

--- installer/driverInstall.js
+++ installer/driverInstall.js
@@ -694,7 +694,10 @@ function findElectronVersion() {
           var codeOut = execSync('code --version').toString();
           vscodeVer = parseFloat(codeOut.split('\n')[0]);
           if(!isNaN(vscodeVer)) {
-            if (vscodeVer >= 1.40) {
+            if (vscodeVer >= 1.43) {
+                electron_version = "7.1.11";
+            }
+            else if (vscodeVer >= 1.40) {
                 electron_version = "6.1.5";
             }
             else if (vscodeVer >= 1.36) {
