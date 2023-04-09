4815c6538de510f4321c17824ff4fe3b51f9c995
Bimal Jha <bjha@rocketsoftware.com>
2023-04-09 07:38:51 -0400

    add char insert test case for issue #915

--- test/test-query-select.js
+++ test/test-query-select.js
@@ -12,6 +12,8 @@ async function main() {
   conn = await ibmdb.open(common.connectionString);
   await testColAlias();
   await testDupColNames();
+  await testColumnAlias();
+  await testCharInsert();
   await conn.close();
   console.log("All tests executed.");
 }
@@ -37,3 +39,25 @@ async function testDupColNames() {
   console.log("Result from testDupColNames= ", data);
 }
 
+async function testColumnAlias() {
+  let sql = "select 'abc' as first, 'dcd' as second, 'efg', 'pqr', 'xyz' as last from SYSIBM.SYSDUMMY1";
+  ibmdb.debug(2);
+  try {
+      data = await conn.querySync(sql);
+  } catch(error) {
+      assert.equal(error, null);
+  }
+  console.log("Result from testColumnAlias= ", data);
+  ibmdb.debug(false);
+}
+
+async function testCharInsert() {
+    await conn.query("create table test1(c1 int, c2 char(5))");
+    await conn.query("insert into test1 values (1, 'abcde')");
+    await conn.query("insert into test1 values (2, 'abcd')");
+    await conn.query("insert into test1 values (?, ?)", [3, 'ABCDE']);
+    await conn.query("insert into test1 values (?, ?)", [4, 'ABCD']);
+    await conn.query("insert into test1 values (?, ?)", [5, { ParamType: 'INPUT', SQLType: 'CHAR', Data: 'DSWC' }]);
+    console.log(conn.querySync("select * from test1"));
+    await conn.query("drop table test1");
+}
