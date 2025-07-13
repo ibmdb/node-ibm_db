1b3549c6660dcbbad0630c5aa1dbad7d24819540
Bimal Jha <bjha@rocketsoftware.com>
2025-07-14 02:28:52 +0530

    add support for electron 37.x

--- installer/driverInstall.js
+++ installer/driverInstall.js
@@ -25,7 +25,7 @@ var platform = os.platform();
 var arch = os.arch();
 
 var vscode_build = false;
-var electron_version = '37.1.0';
+var electron_version = '37.2.1';
 var downloadProgress = 0;
 var silentInstallation = false;
 
@@ -681,9 +681,9 @@ var install_node_ibm_db = function(file_url) {
                 var ODBC_BINDINGS_V17 = 'build\/Release\/odbc_bindings.node.17.9.1';
                 var ODBC_BINDINGS_V18 = 'build\/Release\/odbc_bindings.node.18.20.8';
                 var ODBC_BINDINGS_V19 = 'build\/Release\/odbc_bindings.node.19.9.0';
-                var ODBC_BINDINGS_V20 = 'build\/Release\/odbc_bindings.node.20.19.2';
+                var ODBC_BINDINGS_V20 = 'build\/Release\/odbc_bindings.node.20.19.3';
                 var ODBC_BINDINGS_V21 = 'build\/Release\/odbc_bindings.node.21.7.3';
-                var ODBC_BINDINGS_V22 = 'build\/Release\/odbc_bindings.node.22.16.0';
+                var ODBC_BINDINGS_V22 = 'build\/Release\/odbc_bindings.node.22.17.0';
                 var ODBC_BINDINGS_V23 = 'build\/Release\/odbc_bindings.node.23.11.1';
 
                 // Windows add-on binary for node.js v0.10.x, v0.12.7, 4.x, 6.x to 14.x has been discontinued.
@@ -986,7 +986,7 @@ function findElectronVersion() {
           vscodeVer = parseFloat(codeOut.split('\n')[0]);
           if(!isNaN(vscodeVer)) {
             if (vscodeVer >= 1.101){
-                electron_version = "35.5.1";
+                electron_version = "35.6.0";
             }
             else if (vscodeVer >= 1.100){
                 electron_version = "34.5.1";
